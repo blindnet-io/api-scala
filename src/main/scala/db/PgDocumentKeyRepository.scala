1641516a35df1e523ebf8123a6b5e73ffa45bdc8
Amaury Rousseau <kinrar@kinrar.io>
2022-03-11 05:14:58 +0100

    Fix UUID type handling in SQL queries

--- src/main/scala/db/PgDocumentKeyRepository.scala
+++ src/main/scala/db/PgDocumentKeyRepository.scala
@@ -10,11 +10,11 @@ import doobie.implicits.*
 
 class PgDocumentKeyRepository(xa: Transactor[IO]) extends DocumentKeyRepository[IO] {
   override def findAllByDocument(appId: String, docId: String): IO[List[DocumentKey]] =
-    sql"select app_id, document_id, user_id, enc_sym_key from document_keys where app_id=$appId and document_id=$docId"
+    sql"select app_id, document_id, user_id, enc_sym_key from document_keys where app_id=$appId and document_id=$docId::uuid"
       .query[DocumentKey].to[List].transact(xa)
 
   override def insert(key: DocumentKey): IO[Unit] =
     sql"""insert into document_keys (document_id, user_id, enc_sym_key, app_id)
-          values (${key.documentId}, ${key.userId}, ${key.encSymmetricKey}, ${key.appId})"""
+          values (${key.documentId}::uuid, ${key.userId}, ${key.encSymmetricKey}, ${key.appId})"""
       .update.run.transact(xa).map(_ => ())
 }
--- src/main/scala/db/PgDocumentRepository.scala
+++ src/main/scala/db/PgDocumentRepository.scala
@@ -10,11 +10,11 @@ import doobie.implicits.*
 
 class PgDocumentRepository(xa: Transactor[IO]) extends DocumentRepository[IO] {
   override def findById(id: String): IO[Option[Document]] =
-    sql"select id, app from documents where id=$id"
+    sql"select id, app from documents where id=$id::uuid"
       .query[Document].option.transact(xa)
 
   override def insert(doc: Document): IO[Unit] =
     sql"""insert into documents (id, app)
-          values (${doc.id}, ${doc.appId})"""
+          values (${doc.id}::uuid, ${doc.appId})"""
       .update.run.transact(xa).map(_ => ())
 }
