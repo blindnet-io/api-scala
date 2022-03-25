0819ef5e31fb968abe3018c063a88184593f0a55
Amaury Rousseau <kinrar@kinrar.io>
2022-03-25 10:48:45 +0100

    Fix app repo queries

--- src/main/scala/db/PgAppRepository.scala
+++ src/main/scala/db/PgAppRepository.scala
@@ -12,11 +12,11 @@ import doobie.postgres.implicits.*
 
 class PgAppRepository(xa: Transactor[IO]) extends AppRepository[IO] {
   override def findById(id: String): IO[Option[App]] =
-    sql"select id, public_key, name from apps where id=$id"
+    sql"select id, public_key, name from apps where id=$id::uuid"
       .query[App].option.transact(xa)
 
   override def insert(app: App): IO[Unit] =
     sql"""insert into apps (id, public_key, name)
-          values (${app.id}, ${app.publicKey}, ${app.name})"""
+          values (${app.id}::uuid, ${app.publicKey}, ${app.name})"""
       .update.run.transact(xa).map(_ => ())
 }
