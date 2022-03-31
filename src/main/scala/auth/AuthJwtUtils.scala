5a22d040bd22e669d0fb4cb2b204a1bd5f90e567
Amaury Rousseau <kinrar@kinrar.io>
2022-03-31 12:47:25 +0200

    Jwt: extract base64 decoding to IO function to catch exceptions

--- src/main/scala/auth/AuthJwtUtils.scala
+++ src/main/scala/auth/AuthJwtUtils.scala
@@ -28,22 +28,31 @@ object AuthJwtUtils {
     }
 
   def verifySignatureWithKey(data: Array[Byte], signature: String, key: String): IO[Unit] =
-    parseKey(key) match {
-      case Failure(ex) => IO.raiseError(ex)
-      case Success(parsedKey) =>
-        if JwtUtils.verify(data, Base64.getDecoder.decode(signature), parsedKey, JwtAlgorithm.Ed25519) then IO.unit
-        else IO.raiseError(AuthException("Bad signature"))
+    decodeBase64(signature).flatMap { signatureBytes =>
+      parseKey(key) match {
+        case Failure(ex) => IO.raiseError(ex)
+        case Success(parsedKey) =>
+          if JwtUtils.verify(data, signatureBytes, parsedKey, JwtAlgorithm.Ed25519) then IO.unit
+          else IO.raiseError(AuthException("Bad signature"))
+      }
     }
 
   def verifySignatureWithKey(data: String, signature: String, key: String): IO[Unit] =
     verifySignatureWithKey(data.getBytes, signature, key)
 
   def verifyB64SignatureWithKey(data: String, signature: String, key: String): IO[Unit] =
-    verifySignatureWithKey(Base64.getDecoder.decode(data), signature, key)
+    decodeBase64(data).flatMap(verifySignatureWithKey(_, signature, key))
 
   def parseKey(raw: String): Try[PublicKey] = Try {
     val kf = KeyFactory.getInstance("Ed25519")
     val pubKeyInfo = SubjectPublicKeyInfo(AlgorithmIdentifier(EdECObjectIdentifiers.id_Ed25519), Base64.getDecoder.decode(raw))
     kf.generatePublic(X509EncodedKeySpec(pubKeyInfo.getEncoded))
+  }
+
+  def decodeBase64(data: String): IO[Array[Byte]] = Try {
+    Base64.getDecoder.decode(data)
+  } match {
+    case Failure(ex) => IO.raiseError(ex)
+    case Success(value) => IO.pure(value)
   }
 }
