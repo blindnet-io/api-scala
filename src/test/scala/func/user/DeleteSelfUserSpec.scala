6c0466dbc3025f8fbeac3abbb968c73a43f52442
Amaury Rousseau <kinrar@kinrar.io>
2022-04-07 16:39:45 +0200

    testing: DeleteSelfUser

---
+++ src/test/scala/func/user/DeleteSelfUserSpec.scala
@@ -0,0 +1,91 @@
+package io.blindnet.backend
+package func.user
+
+import util.*
+
+import cats.effect.*
+import com.dimafeng.testcontainers.ContainerDef
+import io.circe.Json
+import io.circe.literal.*
+import io.circe.syntax.*
+import org.http4s.*
+import org.http4s.circe.CirceEntityEncoder.*
+import org.http4s.implicits.*
+import org.scalatest.Assertion
+
+import java.util.UUID
+
+class DeleteSelfUserSpec extends UserAuthEndpointSpec("users/me", Method.DELETE) {
+  override def testValidRequest(): IO[Assertion] = {
+    val testApp = TestApp()
+    val testUser = TestUser()
+
+    for {
+      _ <- testApp.insert(serverApp)
+      _ <- testUser.insert(serverApp, testApp)
+      res <- run(createAuthedRequest(testApp.createUserToken(testUser)))
+      dbUser <- serverApp.userRepo.findById(testApp.id, testUser.id)
+    } yield {
+      assertResult(Status.Ok)(res.status)
+
+      assert(dbUser.isEmpty)
+    }
+  }
+
+  override def testTempUserTokenGid(): IO[Assertion] = {
+    val testApp = TestApp()
+    val testUser = TestUser()
+
+    for {
+      _ <- testApp.insert(serverApp)
+      _ <- testUser.insert(serverApp, testApp)
+      res <- run(createAuthedRequest(testApp.createTempUserToken(testUser.group)))
+    } yield {
+      assertResult(Status.Forbidden)(res.status)
+    }
+  }
+
+  override def testTempUserTokenUids(): IO[Assertion] = {
+    val testApp = TestApp()
+    val testUser = TestUser()
+
+    for {
+      _ <- testApp.insert(serverApp)
+      _ <- testUser.insert(serverApp, testApp)
+      res <- run(createAuthedRequest(testApp.createTempUserToken(List(testUser.id))))
+    } yield {
+      assertResult(Status.Forbidden)(res.status)
+    }
+  }
+
+  override def testClientToken(): IO[Assertion] = {
+    val testApp = TestApp()
+
+    for {
+      _ <- testApp.insert(serverApp)
+      res <- run(createAuthedRequest(testApp.createClientToken()))
+    } yield {
+      assertResult(Status.Forbidden)(res.status)
+    }
+  }
+
+  override def testNoToken(): IO[Assertion] = {
+    for {
+      res <- run(createRequest())
+    } yield {
+      assertResult(Status.Forbidden)(res.status)
+    }
+  }
+
+  it("should fail if user does not exist") {
+    val testApp = TestApp()
+    val testUser = TestUser()
+
+    for {
+      _ <- testApp.insert(serverApp)
+      res <- run(createAuthedRequest(testApp.createUserToken(testUser)))
+    } yield {
+      assertResult(Status.NotFound)(res.status)
+    }
+  }
+}
